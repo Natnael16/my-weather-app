1ecff66520a7ae6ab69800b6bad203ae79a27fe7
natnael16 <natnaeldenbi1@gmail.com>
2025-04-03 15:08:33 +0300

    Fix: Remove unused import for flutter_svg in weather_page.dart

--- lib/features/weather/presentation/screens/weather_page.dart
+++ lib/features/weather/presentation/screens/weather_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import '../bloc/weather_bloc/weather_bloc.dart';
 import '../widgets/responsive_weather_image.dart';
